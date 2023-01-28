05e48674b9301b44d0413167b2c7d044a0c2c76d
plzfgme <m19959605070@163.com>
2023-01-28 22:09:26 +0800

    fix(pg-extension/fastio)

--- pg-extension/pgsse/src/fastio.rs
+++ pg-extension/pgsse/src/fastio.rs
@@ -30,10 +30,7 @@ pub(crate) fn search(prefix: &str, token: &[u8]) -> SetOfIterator<'static, Vec<u
     for i in 1..=c {
         let kw_i = concat_kw_i(kw, i);
         let ui = h1(&kw_i);
-        let e = match te_get(prefix, &ui) {
-            Some(e) => e,
-            None => continue,
-        };
+        let e = te_get(prefix, &ui).expect("index broken");
         let (op, id) = parse_op_id(xor_bytes(e, h2(&kw_i)));
 
         if op == 1 {
